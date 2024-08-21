a956453e6c92b03323538fd62992797ca677a65f
Esly Lescano <eslylescano@gmail.com>
2024-08-22 00:04:49 +0100

    Add handling 405 and 500 not allowed methods

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,24 @@ const app = express();
 app.use(express.json());
 
 app.get('/new', (req, res) => {
-    const width = parseInt(req.query.w as string);
-    const height = parseInt(req.query.h as string);
+    try {
+        const width = parseInt(req.query.w as string);
+        const height = parseInt(req.query.h as string);
 
-    if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
-        return res.status(400).send('Invalid request');
+        if (isNaN(width) || isNaN(height) || width <= 0 || height <= 0) {
+            return res.status(400).send('Invalid request');
+        }
+
+        const state = getInitialState(width, height);
+        res.status(200).json(state);
+    } catch (error) {
+        res.status(500).send('Internal server error');
     }
+});
 
-    const state = getInitialState(width, height);
-    res.status(200).json(state);
+
+app.all('/new', (req, res) => {
+    res.status(405).send('Method Not Allowed');
 });
 
 export default app;
